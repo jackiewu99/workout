5ce09844036b6555288a27b566a5ad3d0058f9fa
elulim <56937083+elulim@users.noreply.github.com>
2022-11-21 21:19:48 -0500

    Add files via upload

--- src/pages/exercise.js
+++ src/pages/exercise.js
@@ -3,7 +3,63 @@ import React from 'react'
 function exercise(){
     return(
     <div>
-        <h1>Exercises</h1>
+        <h1>Dumbbell Exercises</h1>
+
+        <h2>Dumbbell Workout Routines </h2>
+        <h3>The following dumbbell workout routines can be performed using<br/>
+            dumbbell exercises and will allow you to target muscles in a specific<br/>region of your body.
+            They are presented here to highlight how effective working out with<br/> dumbbell exercises can be.</h3>
+        
+        <h2>Dumbbell workout for chest</h2>
+
+        <h3>This workout routine is helpful if you are looking to enlarge your<br/> chest area.
+            It targets the upper, lower, inner and outer pectoral muscles. </h3>
+
+        <h3>The Lying Fly dumbbell exercise will target your inner and outer<br/> pectorals.
+            The Incline Bench Press will target your upper pectorals while the<br/> Decline Bench Press will target your lower pectoral muscles.</h3>
+
+        <h2>Workout Routine for Shoulders</h2>
+
+        <h3>Use this workout routine if you are looking to broaden your shoulders.<br/>
+            It targets the front, outer and rear deltoid muscles.</h3>
+
+        <h3>The Rear Deltoid Raise dumbbell exercise will target your rear deltoid<br/> muscles while
+            the Front Raise will target your front deltoid muscles.</h3>
+
+        <h2>Workout Routine for Arms</h2>
+
+        <h3>Alternated Bicep Curls 3 x 8 Repititions</h3>
+        <h3>Palms-Up wrist curl 2 x 15 Repititions</h3>
+        <h3>Triceps Kickback 3 x 8 Repititions</h3>
+        <h3>Palms-Down Wrist Curl 2 x 15 Repititions</h3>
+
+        <h3>Using this workout routine will help you strengthen the muscles in<br/> your arms.
+            The routine targets the biceps, triceps and forearm muscles.</h3>
+
+        <h3>The Alternated Biceps Curl dumbbell exercise will target your biceps<br/> while the Triceps Kickback is good at isolating your triceps.
+            Palms-Up Wrist Curl will exercise your inner forearms and Palms-Down Wrist<br/> Curls your outer forearm muscles.</h3>
+
+        <h2>Workout Routine for Abdominals</h2>
+
+        <h3>Use this workout routine to strengthen your abdominal muscles (i.e. your abs).<br/>
+            It targets the lower and upper abdominal muscles as well as the obliques.</h3>
+        
+        <h3>The Weighted Crunch and Weighted Leg Raise dumbbell exercises will target your<br/> upper and lower abdominals while
+            the Dumbbell Side Bend will strenghten your obliques.</h3>
+
+        <h2>Workout Routine for the Back</h2>
+
+        <h3>Use this workout routine to strengthen your back muscles.<br/>
+            It targets your lower and upper back as well as your lats.</h3>
+
+        <h2>Workout Routine for Legs</h2>
+        
+        <h3>This routine will be helpful if you want to exercise your leg muscles.<br/>
+            It will target your upper, lower and inner thigh muscles as well as your calves.</h3>
+        
+        <h3>The Side Lunge dumbbell exercise will target your inner thighs while the Toe<br/> Raise targets your calf muscles.</h3>
+
+        <h3>Use this workout routine to strengthen your buttocks. It mainly targets<br/> your glutes.</h3>
     </div>
     );
 }
